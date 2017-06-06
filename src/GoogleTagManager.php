3f83519f0a64b1890716f2a032647f217c4ddaa0
Marcus <info@inklammern.de>
2017-06-06 07:25:46 +0200

    added ability to add a custom script between datalayer and tag

--- src/GoogleTagManager.php
+++ src/GoogleTagManager.php
@@ -13,6 +13,7 @@ class GoogleTagManager
 	private $mustache;
 
 	private $dataLayer = [];
+	private $scripts = [];
 
 	public function __construct()
 	{
@@ -31,11 +32,27 @@ class GoogleTagManager
 		return self::$instance;
 	}
 
+	/**
+	 * Render tag
+	 *
+	 * @param Id $id
+	 * @return string
+	 */
 	public function renderTag(Id $id)
 	{
-		return $this->mustache->render('tag', ['id' => $id, 'dataLayerJson' => json_encode($this->getDataLayerVariables(), JSON_PRETTY_PRINT)]);
+		return $this->mustache->render('tag', [
+			'id' => $id,
+			'customScripts' => implode(PHP_EOL, $this->getCustomScripts()),
+			'dataLayerJson' => json_encode($this->getDataLayerVariables(), JSON_PRETTY_PRINT)
+		]);
 	}
 
+	/**
+	 * Render noscript ag
+	 *
+	 * @param Id $id
+	 * @return string
+	 */
 	public function renderNoScriptTag(Id $id)
 	{
 		$queryParams = ['id' => (string)$id];
@@ -44,11 +61,61 @@ class GoogleTagManager
 		return $this->mustache->render('tag.noscript', ['query' => http_build_query($queryParams)]);
 	}
 
-	public function getDataLayerVariables()
+	/**
+	 * Add js script which is placed after datalayer and before tag
+	 * $name is used as internal identifier
+	 *
+	 * @param string $name
+	 * @param string $script
+	 * @return $this
+	 */
+	public function addCustomScript($name, $script)
 	{
-		return $this->dataLayer;
+		$this->scripts[$name] = $script;
+
+		return $this;
 	}
 
+	/**
+	 * Remove script by name
+	 *
+	 * @param string $name
+	 * @return $this
+	 */
+	public function removeCustomScript($name)
+	{
+		unset($this->scripts[$name]);
+
+		return $this;
+	}
+
+	/**
+	 * Retrieve all scripts
+	 *
+	 * @return array
+	 */
+	public function getCustomScripts()
+	{
+		return $this->scripts;
+	}
+
+	/**
+	 * Remove all scripts
+	 */
+	public function clearCustomScripts()
+	{
+		$this->scripts = [];
+
+		return $this;
+	}
+
+	/**
+	 * Add datalayer variable
+	 *
+	 * @param string $name
+	 * @param string $value
+	 * @return $this
+	 */
 	public function addDataLayerVariable($name, $value)
 	{
 		$this->dataLayer[$name] = $value;
@@ -56,6 +123,12 @@ class GoogleTagManager
 		return $this;
 	}
 
+	/**
+	 * Remove datalayer variable by name
+	 *
+	 * @param string $name
+	 * @return $this
+	 */
 	public function removeDataLayerVariable($name)
 	{
 		unset($this->dataLayer[$name]);
@@ -63,6 +136,21 @@ class GoogleTagManager
 		return $this;
 	}
 
+	/**
+	 * Retrieve all datalayer variables
+	 *
+	 * @return array
+	 */
+	public function getDataLayerVariables()
+	{
+		return $this->dataLayer;
+	}
+
+	/**
+	 * Remove all datalayer variables
+	 *
+	 * @return $this
+	 */
 	public function clearDataLayerVariables()
 	{
 		$this->dataLayer = [];
--- test/GoogleTagManagerTest.php
+++ test/GoogleTagManagerTest.php
@@ -26,23 +26,53 @@ class GoogleTagManagerTest extends \PHPUnit_Framework_TestCase
 	public function testRenderTag()
 	{
 		$id = 123;
+
+		$expectedResult = <<<EOF
+<script>
+var dataLayer = [[]];
+</script>
+
+<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{$id}');
+</script>
+EOF;
+
+		$result = $this->googleTagManager
+			->renderTag(new Id($id));
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public function testRenderTagWithAllFeatures()
+	{
+		$id = 123;
+		$customScript = "<script>console.log('custom_script')</script>";
 		$dataLayerVariableName = 'testName';
 		$dataLayerVariableValue = 'testValue';
 
 		$expectedResult = <<<EOF
 <script>
-var dataLayer = [{"testName":"testValue"}];
-
+var dataLayer = [{
+    "{$dataLayerVariableName}": "{$dataLayerVariableValue}"
+}];
+</script>
+{$customScript}
+<script>
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','123');
+})(window,document,'script','dataLayer','{$id}');
 </script>
 EOF;
 
 		$result = $this->googleTagManager
 			->addDataLayerVariable($dataLayerVariableName, $dataLayerVariableValue)
+			->addCustomScript('test', $customScript)
 			->renderTag(new Id($id));
 
 		$this->assertSame($expectedResult, $result);
@@ -55,7 +85,7 @@ EOF;
 		$dataLayerVariableValue = 'testValue';
 
 		$expectedResult = <<<EOF
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=123&amp;testName=testValue" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={$id}&amp;{$dataLayerVariableName}={$dataLayerVariableValue}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 EOF;
 
 		$result = $this->googleTagManager
